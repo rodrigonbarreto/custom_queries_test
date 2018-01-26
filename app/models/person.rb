1faeae10df1924bade475339d61e3f328c60e8ec
Rodrigo Barreto <rodrigonbarreto@gmail.com>
2018-01-26 16:50:12 +0100

    custom query and aggregations query

--- app/models/person.rb
+++ app/models/person.rb
@@ -13,4 +13,93 @@ class Person < ApplicationRecord
     joins(:location).merge(Location.joins(:region).merge(Region.order(:name)).order(:name)).order(:name)
   end
 
+  ## has many
+
+
+
+
+  def self.with_employees_order_by_location_name
+    from(self.order_by_location, :people).order(:name).distinct
+
+    #OR
+    # Person.from(Person.with_employees, :people).order_by_location_name
+  end
+
+  def self.order_by_location
+    joins(:location, :employees).merge(Location.order(:name))
+  end
+
+  #custom joins
+
+  def self.without_remote_manager
+    # joins("LEFT JOIN people managers ON managers.id = people.manager_id").
+    # where("people.manager_id IS NULL or people.location_id = managers.location_id")
+    #
+    joins(<<-SQL).
+      LEFT JOIN people managers
+      ON managers.id = people.manager_id
+    SQL
+    where('people.manager_id IS NULL OR managers.name = ?', "local_manager")
+  end
+
+  def self.order_by_location_name
+    joins(:location).order("locations.name")
+  end
+
+  ## ORDER USING MERGE
+  # def self.order_by_location_name
+  #   joins(:location).merge(Location.order(:name)).order(:name)
+  # end
+
+  def self.with_employees
+    ## using from
+    # from(
+    #     joins(:employees).distinct,
+    #     :people
+    # )
+    #
+    ##  Like more this option
+    Person.from(joins(:employees).distinct, :people)
+  end
+
+  #Other ways to do some queries
+  # def self.with_employees
+  #   joins(:employees).distinct
+  #
+  #   # maybe under is better
+  #   # joins(employees: :manager).merge(Person.order(:name)).distinct
+  #   # or
+  #   # where(id: Person.pluck(:manager_id)).distinct
+  # end
+
+  def self.with_local_coworkers
+    from(joins(location: :people).where("people_locations.id <> people.id").distinct, :people)
+  end
+
+  #aggregations
+  def self.maximum_salary_by_location
+    # #option 1
+    # joins(:location)
+    #     .group('locations.id')
+    #     .maximum(:salary)
+
+    #option 2
+    group(:location_id).maximum(:salary)
+
+  end
+
+  def self.managers_by_average_salary_difference
+    joins(
+        'INNER JOIN (' +
+            Person.
+                joins('INNER JOIN people employees ON employees.manager_id = people.id').
+                select('people.id, AVG(employees.salary) as average_salary').
+                group('people.id').to_sql +
+            ') managers ON managers.id = people.id'
+    ).
+        select('people.name, (people.salary - managers.average_salary) as salary_diff').
+        order('salary_diff DESC')
+
+  end
+
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -54,4 +54,255 @@ RSpec.describe Person, type: :model do
     end
   end
 
+
+  ##HAS MANY
+
+  describe ".order_by_location_name" do
+    it "groups people by location" do
+
+      locations = [
+          create(:location, name: "location1"),
+          create(:location, name: "location3"),
+          create(:location, name: "location2")
+      ]
+      locations.each do |location|
+        create(:person, location: location, name: "at-#{location.name}")
+      end
+
+      result = Person.order_by_location_name
+
+      expect(result.map(&:name)).
+          to eq(%w(at-location1 at-location2 at-location3))
+    end
+  end
+
+  describe ".with_employees" do
+    it "finds people who manage employees" do
+
+      managers = [
+          create(:person, name: "manager-one"),
+          create(:person, name: "manager-two")
+      ]
+      managers.each do |manager|
+        2.times do
+          create(:person, name: "employee-of-#{manager.name}", manager: manager)
+        end
+      end
+
+      result = Person.with_employees
+
+      expect(result.map(&:name)).to match_array(%w(manager-one manager-two))
+    end
+  end
+
+  describe ".with_employees_order_by_location_name" do
+    it "finds managers ordered by location name" do
+      # pending "Fix this spec last"
+
+      locations = [
+          create(:location, name: "location1"),
+          create(:location, name: "location3"),
+          create(:location, name: "location2")
+      ]
+      managers = locations.map do |location|
+        create(:person, name: "manager-at-#{location.name}", location: location)
+      end
+      managers.each do |manager|
+        2.times do
+          create(:person, name: "employee-of-#{manager.name}", manager: manager)
+        end
+      end
+
+      result = Person.with_employees_order_by_location_name
+
+      expect(result.map(&:name)).to eq(%w(
+        manager-at-location1
+        manager-at-location2
+        manager-at-location3
+      ))
+    end
+  end
+
+  ## END HAS MANY
+  #
+
+  ##START CUSTOM
+
+  describe "#without_remote_manager" do
+    it "returns people who have no manager or whose manager is local" do
+      #pending "Implement without_remote_manager to make this spec pass"
+
+      local = create(:location)
+      remote = create(:location)
+      local_manager = create(
+          :person,
+          location: local,
+          name: "local_manager",
+          manager: nil
+      )
+      remote_manager = create(
+          :person,
+          location: remote,
+          name: "remote_manager",
+          manager: nil
+      )
+      create(
+          :person,
+          location: local,
+          manager: local_manager,
+          name: "has_local_manager"
+      )
+      create(
+          :person,
+          location: local,
+          manager: remote_manager,
+          name: "has_remote_manager"
+      )
+
+      result = Person.without_remote_manager
+
+      expect(result.map(&:name)).
+          to match_array(%w(local_manager remote_manager has_local_manager))
+    end
+  end
+
+  describe ".order_by_location_name" do
+    it "groups people by location" do
+      locations = [
+          create(:location, name: "location1"),
+          create(:location, name: "location3"),
+          create(:location, name: "location2")
+      ]
+      locations.each do |location|
+        create(:person, location: location, name: "at-#{location.name}")
+      end
+
+      result = Person.order_by_location_name
+
+      expect(result.map(&:name)).
+          to eq(%w(at-location1 at-location2 at-location3))
+    end
+  end
+
+  describe ".with_employees" do
+    it "finds people who manage employees" do
+      managers = [
+          create(:person, name: "manager-one"),
+          create(:person, name: "manager-two")
+      ]
+      managers.each do |manager|
+        2.times do
+          create(:person, name: "employee-of-#{manager.name}", manager: manager)
+        end
+      end
+
+      result = Person.with_employees
+
+      expect(result.map(&:name)).to match_array(%w(manager-one manager-two))
+    end
+  end
+
+  describe ".with_local_coworkers" do
+    it "finds people with other people at the same location" do
+      location = create(:location)
+      other_location = create(:location)
+      create(:person, location: location, name: "with-coworkers-one")
+      create(:person, location: location, name: "with-coworkers-two")
+      create(:person, location: location, name: "with-coworkers-three")
+      create(:person, location: other_location, name: "without-coworkers")
+
+      result = Person.with_local_coworkers
+
+      expect(result.map(&:name)).to match_array(%w(
+        with-coworkers-one
+        with-coworkers-two
+        with-coworkers-three
+      ))
+    end
+  end
+
+  describe ".with_employees.with_local_coworkers.order_by_location_name" do
+    it "combines scopes" do
+      locations = [
+          create(:location, name: "location1"),
+          create(:location, name: "location3"),
+          create(:location, name: "location2")
+      ]
+      managers = locations.map do |location|
+        create(:person, name: "coworker-#{location.name}", location: location)
+        create(:person, name: "manager-#{location.name}", location: location)
+      end
+      managers.each do |manager|
+        2.times do
+          create(:person, name: "employee-#{manager.name}", manager: manager)
+        end
+      end
+
+      result = Person.with_employees.with_local_coworkers.order_by_location_name
+
+      expect(result.map(&:name)).to eq(%w(
+        manager-location1
+        manager-location2
+        manager-location3
+      ))
+    end
+  end
+
+  # END CUSTOM
+  #
+
+  #START Aggregations
+  describe ".maximum_salary_by_location" do
+    it "finds the highest salary at each location" do
+      # pending "Implement maximum_salary_by_location to make this spec pass"
+
+      [50_000, 60_000].each do |highest_salary|
+        location = create(:location, name: "highest-#{highest_salary}")
+        create(:person, location: location, salary: highest_salary - 1)
+        create(:person, location: location, salary: highest_salary)
+      end
+
+      result = Person.maximum_salary_by_location
+
+      expect(find_names(result)).to eq(
+                                        "highest-50000" => 50_000,
+                                        "highest-60000" => 60_000
+                                    )
+    end
+  end
+
+  def find_names(hash_by_id)
+    hash_by_id.inject({}) do |hash_by_name, (id, value)|
+      name = Location.find(id).name
+      hash_by_name.merge(name => value)
+    end
+  end
+
+  describe ".managers_by_average_salary_difference" do
+    it "orders managers by the difference between their salary and the average salary of their employees" do
+      # pending "Implement managers_by_average_salary_difference to make this spec pass"
+
+      highest_difference = [45_000, 20_000]
+      medium_difference = [50_000, 10_000]
+      lowest_difference = [50_000, -5_000]
+      ordered_differences = [highest_difference, medium_difference, lowest_difference]
+
+      ordered_differences.each do |(salary, difference)|
+        manager = create(:person, salary: salary, name: "difference-#{difference}")
+        create(:person, manager: manager, salary: salary - difference * 1)
+        create(:person, manager: manager, salary: salary - difference * 2)
+        create(:person, manager: manager, salary: salary - difference * 3)
+      end
+
+      result = Person.managers_by_average_salary_difference
+
+      expect(result.map(&:name)).to eq(%w(
+        difference-20000
+        difference-10000
+        difference--5000
+      ))
+    end
+  end
+  # END
+
 end
